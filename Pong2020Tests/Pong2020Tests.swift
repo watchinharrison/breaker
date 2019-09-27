d847c92a4679495c9f0e18d47cd063b6b96bef75
David Harrison <david.harrison@ladbiblegroup.com>
2019-09-27 22:13:27 +0100

    Initial Commit

---
+++ Pong2020Tests/Pong2020Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Pong2020Tests.swift
+//  Pong2020Tests
+//
+//  Created by David Harrison on 27/09/2019.
+//  Copyright © 2019 David Harrison. All rights reserved.
+//
+
+import XCTest
+@testable import Pong2020
+
+class Pong2020Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
